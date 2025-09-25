ac0095c8b821557e827c3bf3f61845938b82a4c7
miloszmichalak <miloszmichalak150@gmail.com>
2025-09-25 15:08:36 +0200

    Dodano zadanie z brylami

--- X.09.25/18.09/fibonacci/src/App.js
+++ X.09.25/18.09/fibonacci/src/App.js
@@ -4,27 +4,27 @@ import { useState } from 'react';
 
 function fib(n) {
   if (n < 1) return [];
-  let current = 1, previous = 0;
-  const sequence = [1];
+  let obecny = 1, poprzedni = 0;
+  const ciag = [1];
 
   for (let i = 1; i < n; i++) {
-    const temp = current;
-    current += previous;
-    previous = temp;
-    sequence.push(current);
+    const temp = obecny;
+    obecny += poprzedni;
+    poprzedni = temp;
+    ciag.push(obecny);
   }
 
-  return sequence;
+  return ciag;
 }
 
 
-function multiplyFib(arr) {
-  let product = 1;
+function pomnozFib(arr) {
+  let iloczyn = 1;
   arr.forEach(element => {
-    product *= element;
+    iloczyn *= element;
   });
 
-  return product;
+  return iloczyn;
 }
 
 export default function App() {
@@ -39,7 +39,7 @@ export default function App() {
         <input type='number' onChange={e => setN(parseInt(e.target.value))} placeholder='Podaj n'/>
         <input type='button' onClick={() => setResult(fib(n))} value='Oblicz'/>
         <p>N elementow fibo: {result.join(" * ")}</p>
-        <p>Iloczyn: {multiplyFib(result)}</p>
+        <p>Iloczyn: {pomnozFib(result)}</p>
       </header>
     </div>
   );
---
+++ X.09.25/25.09/zadaniebryly/src/App.js
@@ -0,0 +1,133 @@
+import './App.css';
+import szescian from './assets/szescian.png'
+import prostopadloscian from './assets/prostopadloscian.png'
+import kula from './assets/kula.jpg'
+// import { useState } from 'react';
+
+function obliczPoleSzescianu(a){
+  return 6 * Math.pow(a, 2);
+}
+
+function obliczPoleProstopadloscianu(a, b, h){
+  return 2 * a * b + 2 * a * h + 2 * b * h;
+}
+
+function obliczPoleKuli(r){
+ return 4 * Math.PI * Math.pow(r, 2);
+}
+
+
+function obliczObjetoscSzescianu(a){
+  return Math.pow(a, 3);  
+}
+
+function obliczObjetoscProstopadloscianu(a, b, h){
+    return a * b * h;
+}
+
+function obliczObjetoscKuli(r){
+   return (4/3) * Math.PI * Math.pow(r, 3);
+}
+
+
+function changeValueToList(pole, objetosc) {
+  return [{"pole":pole, "objetosc":objetosc}]
+}
+
+function getProperValue(name, lista){
+  switch (name) {
+    case "szescian":
+      return changeValueToList(obliczPoleSzescianu(lista[0].wymiar), obliczObjetoscSzescianu(lista[0].wymiar));
+    case "prostopadloscian":
+      return changeValueToList(obliczPoleProstopadloscianu(lista[0].wymiar, lista[1].wymiar, lista[2].wymiar), obliczObjetoscProstopadloscianu(lista[0].wymiar, lista[1].wymiar, lista[2].wymiar));
+    case "kula":
+      return changeValueToList(obliczPoleKuli(lista[0].wymiar), obliczObjetoscKuli(lista[0].wymiar));
+    default:
+      return "error";
+  }
+}
+
+
+const bryly = [
+  {"id":0,"nazwa":"szescian","ilustracja":szescian  ,
+    "wzory":[
+      {"id":0,"wzor":"V=a^3"},
+      {"id":1,"wzor":"P=6a^2"},
+    ],
+    "wymiary": [
+      {"id":0, "wymiar":2}
+    ]
+  },
+  {"id":1,"nazwa":"prostopadloscian","ilustracja":prostopadloscian,
+    "wzory":[
+      {"id":0,"wzor":"V=a*b*h"},
+      {"id":1,"wzor":"P=2*a*b+2*a*h+2*b*h"},
+    ],
+    "wymiary": [
+      {"id":0, "wymiar":3},
+      {"id":1, "wymiar":4},
+      {"id":2, "wymiar":5},
+    ]
+  },
+  {"id":2,"nazwa":"kula","ilustracja":kula,
+    "wzory":[
+      {"id":0,"wzor":"V=4/3*pi*r^3"},
+      {"id":1,"wzor":"P=4*pi*r^2"},
+    ],
+    "wymiary": [
+      {"id":0, "wymiar":4}
+    ]
+  },
+];
+
+export default function App() {
+  // const [measurments, setMeasurments] = useState([]);
+
+  return (
+    <div className="App">
+    <h1>Bryly</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>BRYLA</th>
+            <th>ILUSTRACJA</th>
+            <th>WZORY</th>
+            <th>PRZYKLAD</th>
+          </tr>
+        </thead>
+        <tbody>
+          { 
+            bryly.map( (item, index) => (
+              <tr key={item.id}>
+                <td>
+                  <p className='name'>{item.nazwa}</p>
+                </td>
+                <td>
+                  <img src={item.ilustracja} alt='Zdjecie bryly'></img>
+                </td>
+                <td>
+                  <p className='wzor'>
+                    Objetosc: {item.wzory[0].wzor} <br></br>
+                    Pole: {item.wzory[1].wzor}
+                  </p>
+                </td>
+                <td>
+                  <ol>
+                    {
+                      item.wymiary.map((itemW, index) => (
+                        <li key={index} className='measurement'>Wymiar: {itemW.wymiar}</li>
+                      ))
+                    }
+                    <p className='values'>
+                    Pole: {getProperValue(item.nazwa, item.wymiary)[0].pole} <br></br> 
+                    Objetosc: {getProperValue(item.nazwa, item.wymiary)[0].objetosc}</p>
+                  </ol>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
